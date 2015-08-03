6be0a5c6b8aefdcd349471098dcfa0c09eeeac12
Andrew Frank <andrew.frank@wiredwithpurpose.com>
2015-08-03 12:08:00 -0400

    Added a find_longest_chain method.

--- lib/collatz.rb
+++ lib/collatz.rb
@@ -1,4 +1,12 @@
 class Collatz
+  def self.find_longest_chain(maximum)
+    chains = maximum.times.map {|n| n + 1 }.inject({}){ |m, n|
+      m[n] = calculate_chain(n)
+      m
+    }
+    chains.max { |a, b| a.last.count <=> b.last.count }.first
+  end
+
   def self.calculate_chain(seed)
     current_number = seed
     results = []
--- spec/collatz_spec.rb
+++ spec/collatz_spec.rb
@@ -5,4 +5,9 @@ describe Collatz do
     When(:result) { Collatz.calculate_chain(13) }
     Then { result == [13, 40, 20, 10, 5, 16, 8, 4, 2, 1] }
   end
+
+  describe '.find_longest_chain' do
+    When(:result) { Collatz.find_longest_chain(5) }
+    Then { result == 3 }
+  end
 end
